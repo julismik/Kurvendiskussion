59b2e8afebeb35423061af342ccd85ecd776ee07
轶桐 吴 <3420675312@qq.com>
2022-03-28 14:53:24 +0200

    Projektdateien hinzufügen.

---
+++ KurveliniareF.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurvendiskussion
+{
+    class KurveliniareF
+    {
+        //Eigenschaften
+        public double Anstieg {get; set; }
+        public double Verschiebung { get; set; }
+        
+
+      //Methoden
+      //Konstruktor ist e ine Methode die genau so heisst wie die klasse
+       public KurveliniareF(double anstieg, double verschiebung) // diese variable existern nur im Konstruktor
+        {
+            this.Anstieg = anstieg; // this bedeutet dass variable aus classe kommt == (anstieg = m)
+            this.Verschiebung = verschiebung;
+            
+        }
+
+        // Weitere Methode
+        public double berechnenNullstelle ()
+        {
+            double nullstelle;
+            nullstelle = -Verschiebung / Anstieg;
+            return nullstelle;
+        }
+        public double berechnenFunktionswert (double Punkt)
+        {
+            double wert;
+            wert = Anstieg * Punkt + Verschiebung;
+            return wert;
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kurvendiskussion
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double anstieg = Convert.ToDouble(A.Text);
+            double verschiebung = Convert.ToDouble(B.Text);
+            KurveliniareF f = new KurveliniareF(anstieg, verschiebung);
+            
+            string Ausgabe;
+            Ausgabe = "Die Nullstelle ist x_n = " + f.berechnenNullstelle() + "\n";
+            Ausgabe += " Der Schnittpunkt mi der xAchse liegt bei: (" + f.berechnenNullstelle() + "/0" ;
+            Ergebnis.Text = Ausgabe;
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            double anstieg = Convert.ToDouble(A.Text);
+            double verschiebung = Convert.ToDouble(B.Text);
+            double punkt = Convert.ToDouble(Punkt.Text);
+            KurveliniareF f = new KurveliniareF(anstieg, verschiebung);
+           
+           
+            Wert.Text = Convert.ToString(f.berechnenFunktionswert(Convert.ToDouble(Punkt.Text)));
+        }
+    }
+}
